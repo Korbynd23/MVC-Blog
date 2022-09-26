f6518e208be89528ca72ffd3e41a713204e00484
kdicari24@gmail.com <kdicari24@gmail.com>
2022-09-26 18:46:51 -0400

    saving changes for push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
@@ -40,5 +40,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+  app.listen(port, () => console.log(`Now listening on ${port}`));
 });
